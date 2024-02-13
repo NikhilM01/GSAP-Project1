8d0e5e58fe6e348ba7fc5d2ad09399d74d8c72a0
Nikhil Muneshwar <93129551+NikhilM01@users.noreply.github.com>
2024-02-13 14:01:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var t1 = gsap.timeline();
+
+t1.from("#nav h3", {
+  y: -50,
+  opacity: 0,
+  delay: 0.4,
+  duration: 0.8,
+  stagger: 0.3,
+});
+
+t1.from("#main h1", {
+  x: -500,
+  opacity: 0,
+  duration: 0.8,
+  stagger: 0.4,
+});
+
+t1.from("img", {
+  x: 100,
+  rotate: 45,
+  opacity: 0,
+  duration: 0.6,
+  stagger: 0.5,
+});
+
+t1.from("#bottom h2", {
+  y: -10,
+  opacity: 0,
+  delay: 0.4,
+  duration: 0.8,
+  stagger: 0.3,
+});
